8f02045f3fac2113a40b74df50c91de3743e7d57
AlmirJrDev <almagomes1@hotmail.com>
2022-11-28 16:16:58 -0300

    problemas

--- src/components/TaskNew.jsx
+++ src/components/TaskNew.jsx
@@ -1,9 +1,26 @@
 import styles from './TaskNew.module.css'
 
+import circle from '../assets/circle.svg'
+
+import circle2 from '../assets/circle2.svg'
+
+
+
+function checkBox() {
+
+
+   if(check.src === {circle})
+    check.src = {circle2}
+
+}
+
+
+
+
 export function TaskNew() {
     return(
         <div className={styles.taskInfo}>
-            <input className={styles.taskCheck} type="checkbox" name="" id="" />
+            <img onClick={checkBox} id='check' src={circle} alt="" />
             <label ></label>
             <p className={styles.infoParagraph}>Olá</p>
         </div>
